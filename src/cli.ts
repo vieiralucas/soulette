27d0d8964f622e50f25da18b7794a61dc5f824c5
Lucas Vieira <vieiralucas4@gmail.com>
2022-04-22 16:54:37 -0300

    feat: read wallte from ~/.config

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,22 @@
 import { Keypair } from '@solana/web3.js'
 import chalk from 'chalk'
 import figlet from 'figlet'
+import fs from 'fs'
 import * as inquirer from 'inquirer'
+import os from 'os'
+import path from 'path'
 
 import { getReturnAmount, totalAmtToBePaid, randomNumber } from './helper'
 import { getWalletBalance, transferSOL, airDropSol } from './solana'
+
+//Treasury
+const secretKey = [
+  111, 188, 76, 169, 30, 105, 254, 33, 228, 66, 56, 215, 9, 37, 51, 188, 188,
+  188, 20, 224, 228, 115, 17, 163, 151, 105, 113, 251, 105, 177, 28, 157, 125,
+  202, 195, 203, 253, 137, 26, 209, 7, 2, 66, 193, 76, 241, 203, 168, 213, 5,
+  226, 11, 142, 44, 125, 191, 167, 172, 166, 207, 176, 137, 210, 27,
+]
+const treasuryWallet = Keypair.fromSecretKey(Uint8Array.from(secretKey))
 
 const init = () => {
   console.log(
@@ -26,33 +38,6 @@ const init = () => {
 //Generate a Random Number
 //Ask for the generated Number
 //If true return the SOL as per ratio
-
-// const userWallet=web3.Keypair.generate();
-
-// const userPublicKey=[
-//     6,  85, 188,  71, 255,  12, 214, 102,
-//    84, 170, 129, 127,  64,  57, 133,  22,
-//    10,   9, 135,  34,  75, 223, 107, 252,
-//   253,  22, 242, 135, 180, 245, 221, 155
-// ]
-const userSecretKey = [
-  229, 65, 12, 110, 128, 101, 62, 119, 239, 95, 26, 67, 178, 99, 40, 77, 46,
-  151, 163, 227, 167, 5, 138, 101, 140, 195, 212, 161, 105, 216, 79, 73, 6, 85,
-  188, 71, 255, 12, 214, 102, 84, 170, 129, 127, 64, 57, 133, 22, 10, 9, 135,
-  34, 75, 223, 107, 252, 253, 22, 242, 135, 180, 245, 221, 155,
-]
-
-const userWallet = Keypair.fromSecretKey(Uint8Array.from(userSecretKey))
-
-//Treasury
-const secretKey = [
-  111, 188, 76, 169, 30, 105, 254, 33, 228, 66, 56, 215, 9, 37, 51, 188, 188,
-  188, 20, 224, 228, 115, 17, 163, 151, 105, 113, 251, 105, 177, 28, 157, 125,
-  202, 195, 203, 253, 137, 26, 209, 7, 2, 66, 193, 76, 241, 203, 168, 213, 5,
-  226, 11, 142, 44, 125, 191, 167, 172, 166, 207, 176, 137, 210, 27,
-]
-
-const treasuryWallet = Keypair.fromSecretKey(Uint8Array.from(secretKey))
 
 const askQuestions = (balance: number) => {
   const questions: inquirer.QuestionCollection = [
@@ -107,7 +92,23 @@ const askQuestions = (balance: number) => {
   return inquirer.prompt(questions)
 }
 
+const loadUserWallet = async (): Promise<Keypair> => {
+  const fromConfig = Uint8Array.from(
+    JSON.parse(
+      (
+        await fs.promises.readFile(
+          path.join(os.homedir(), '.config', 'solana', 'id.json')
+        )
+      ).toString()
+    )
+  )
+
+  return Keypair.fromSecretKey(Uint8Array.from(fromConfig))
+}
+
 const gameExecution = async () => {
+  const userWallet = await loadUserWallet()
+
   init()
   const generateRandomNumber = randomNumber(1, 5)
 
