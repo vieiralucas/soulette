b72d9926becee0ef0b746e64c165f7dd6f90a8d8
Lucas Vieira <vieiralucas4@gmail.com>
2022-04-29 20:50:43 -0300

    chore: add material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
+import { EmotionCache } from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider, CssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
+import React from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import lightTheme from '../styles/theme/lightTheme'
+import createEmotionCache from '../utility/createEmotionCache'
+
+const clientSideEmotionCache = createEmotionCache()
+
+const MyApp = (props: AppProps<{ emotionCache?: EmotionCache }>) => {
+  const { Component, pageProps } = props
+  const { emotionCache = clientSideEmotionCache } = pageProps
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
